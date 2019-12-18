20c2b18346dcc9ac493555efba4e09e76a0cbef8
LaoHsia <a4890894@gmail.com>
2019-12-18 14:32:34 +0800

    complete

--- app.py
+++ app.py
@@ -67,13 +67,13 @@ def handle_message(event):
     if "我要預訂" in msg:
         carousel_template = CarouselTemplate(columns=[
             CarouselColumn(thumbnail_image_url='https://scontent.ftpe8-2.fna.fbcdn.net/v/t1.0-9/78858712_3020675774617584_8482206259481673728_n.jpg?_nc_cat=103&_nc_ohc=LcqCtsc9Mg4AQkWAfO3bHQkSFkQehfvv6JcB8Uph07V7mgieZ6G3k749w&_nc_ht=scontent.ftpe8-2.fna&oh=d0720b979ae3ae5e8164f4c3f7bcb2b1&oe=5E7D327A',
-                           text='請填寫表單，完成後來電確認到貨日期，取貨時付款即可', title='台中高鐵站門市(自取)',
+                           text='請填寫表單，完成後來電確認到貨日期，取貨時付款即可\nTime : 09:00-21:00\nTEL : 0905-675038', title='台中高鐵站門市(自取)',
                            actions=[
                 URIAction(label='訂購表單', uri='https://bit.ly/2Lhgrnx'),
                 MessageAction(label='門市地址', text='高鐵門市地址')
             ]),
             CarouselColumn(thumbnail_image_url='https://scontent.ftpe8-2.fna.fbcdn.net/v/t1.0-9/79327003_3020675734617588_111192146333138944_n.jpg?_nc_cat=103&_nc_ohc=TtWIs9eKc-sAQmpge1FFJnBDoRemxWS3a0-LVR6sHLhT35_mS8IKxTsXg&_nc_ht=scontent.ftpe8-2.fna&oh=27b81efab9bcbdf4f8d92525515d998b&oe=5EB39E9E',
-                           text='請填寫表單，完成後來電確認到貨日期，取貨時付款即可', title='美村門市(自取)',
+                           text='請填寫表單，完成後來電確認到貨日期，取貨時付款即可\nTime : 09:00-21:00\nTEL : 0905-675038', title='美村門市(自取)',
                            actions=[
                 URIAction(label='訂購表單', uri='https://bit.ly/2sAgS4I'),
                 MessageAction(label='門市地址', text='美村本店地址')
